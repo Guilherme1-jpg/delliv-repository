ca9debeca55d415a8eac25f6c20d362b23e66dd7
Guilherme Henrique <henriqueoliveiragui18@gmail.com>
2024-01-12 10:51:50 -0300

    Update server.js

--- frontend/src/api/server.js
+++ frontend/src/api/server.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const baseApi = axios.create({
-  baseURL: 'http://localhost:3002/',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 baseApi.interceptors.request.use(
